a9d702fa3ca9ecdf34cc79836b7e0b67bbb0bc39
Reshani Dilhari <56593064+ReshaniD97@users.noreply.github.com>
2021-08-16 14:17:51 +0530

    AddCardsVaccineManagerReports

--- frontend/src/components/VaccineManager/SubComponenets/VaccinatedCalender.js
+++ frontend/src/components/VaccineManager/SubComponenets/VaccinatedCalender.js
@@ -2,11 +2,47 @@ import React from "react";
 import { DateRangePicker } from "rsuite";
 import "../../../styles/Datepicker.css";
 import "rsuite/dist/styles/rsuite-default.css";
+import "../../../styles/VaccineMangerCard.css";
 
 const VaccinatedCalender = () => {
   return (
-    <div className="dates">
-      <DateRangePicker />
+    <div>
+      <div className="dates">
+        <DateRangePicker />
+      </div>
+      <div
+        class="rows1"
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+      >
+        <div class="columns1">
+          <div class="cards1">
+            <h5>1st Dose Vaccinated People</h5>
+            <h4>1936</h4>
+          </div>
+        </div>
+
+        <div class="columns1">
+          <div class="cards1">
+            <h5>2d Dose Vaccinated People</h5>
+            <h4>1936</h4>
+          </div>
+        </div>
+
+        <div class="columns1">
+          <div class="cards1">
+            <h5>1st & 2nd Doses Vaccinated People</h5>
+            <h4>1936</h4>
+          </div>
+        </div>
+
+        <div class="columns1">
+          <div class="cards1">
+            <h5>No.of Registered People</h5>
+            <h4>1936</h4>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
