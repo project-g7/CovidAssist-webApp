e14cd30b520e12d1074162142660fab978ab3ad3
nuwansasanka <nuwansasanka1@gmail.com>
2021-09-11 20:49:26 +0530

    vaccinedashboard

---
+++ frontend/src/components/VaccineManager/SubComponenets/Dashboard/DashboardCard2.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const DashboardCard2 = () => {
+  const [iotPlaces, setIotPlaces] = useState(0);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3002/getiotcount")
+      .then((res) => {
+        console.log(res.data);
+        setIotPlaces(res.data[0].iotCount);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <div class="card">
+      <b>
+        <p class="dash">Public IOT Places</p>
+      </b>
+      <b>
+        <p class="infor">{iotPlaces}</p>
+      </b>
+    </div>
+  );
+};
+
+export default DashboardCard2;
---
+++ frontend/src/components/VaccineManager/SubComponenets/Dashboard/DashboardCard4.js
@@ -0,0 +1,30 @@
+import React,{ useState, useEffect }  from "react";
+import axios from "axios";
+
+const DashboardCard4 = () => {
+  const [users, setUsers] = useState("");
+  useEffect(() => {
+    axios
+      .get("http://localhost:3002/getuserscount")
+      .then((res) => {
+        console.log(res.data);
+        setUsers(res.data[0].userCount)
+
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <div class="card">
+      <b>
+        <p class="dash">Total Mobile App users</p>
+      </b>
+      <b>
+        <p class="infor">{users}</p>
+      </b>
+    </div>
+  );
+};
+
+export default DashboardCard4;
