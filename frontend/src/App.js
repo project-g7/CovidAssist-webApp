7f747209d632e7de7c31caf0728b78b7c313d461
nuvin0214 <64645538+nuvin0214@users.noreply.github.com>
2021-09-15 10:11:10 +0530

    nuvin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,22 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
 import Login from "./components/Login";
 import Welcome from "./components/Welcome";
 import WelcomeAdmin from "./components/Admin/WelcomeAdmin";
 import AdminVaccineManage from "./components/Admin/pages/VaccineManage";
 import DisplayCenter from "./components/Admin/pages/DisplayCenter";
 import Requests from "./components/Admin/pages/Requests";
-import Profile from "./components/Admin/pages/Profile";
-import vaccineProfile from "./components/VaccineManager/pages/Profile";
-import ctProfile from "./components/ContactTracingManager/pages/Profile";
 import Iot from "./components/Admin/pages/Iot";
 import WelcomeVaccine from "./components/VaccineManager/WelcomeVaccine";
 import WelcomeContact from "./components/ContactTracingManager/WelcomeContactTracing";
 import ForgotPassword from "./components/ForgotPassword";
 import Register from "./components/Register";
-import { BrowserRouter as Router, Link, Route, Switch,Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import { VaccineBooking } from "./components/Admin/pages/Reports/VaccineBooking";
 import VaccinationAreas from "./components/Admin/pages/Reports/VaccinationAreas";
 import HealthMeasures from "./components/Admin/pages/Reports/HealthMeasures";
 import VaccinatedDetails from "./components/VaccineManager/Reports/VaccinatedDetails";
-import ReservedVaccineList from "./components/VaccineManager/pages/ReservedVaccineList";
-import Upcomingbookings from "./components/VaccineManager/pages/Upcomingbookings"; 
-import RegisterDetails from "./components/VaccineManager/pages/RegisterDetails";
-import UpcomingRegisterDetails from "./components/VaccineManager/pages/UpcomingRegisterDetails";
-import CheckBookingDetails from "./components/VaccineManager/pages/CheckBookingDetails";
-// import WelcomeVaccine from "./components/Vaccine/WelcomeVaccine";
-
+import ContactTable from "./components/ContactTracingManager/SubComponenets/ContactTable";
 function App() {
-  const [loggedIn,setLoggedIn] = useState(true);
   return (
     <Router>
       <Switch>
@@ -70,47 +59,7 @@ function App() {
           path="/admin/vaccinemanage/center"
           component={DisplayCenter}
         ></Route>
-         <Route
-          exact
-          path="/vaccine/vaccinelist"
-          component={ReservedVaccineList}
-        ></Route>
-        <Route
-          exact
-          path="/admin/profile"
-          component={Profile}
-        ></Route>
-        <Route
-          exact
-          path="/vaccine/profile"
-          component={vaccineProfile}
-        ></Route>
-        <Route
-          exact
-          path="/ct/profile"   
-          component={ctProfile}
-        ></Route>
-         <Route
-          exact
-          path="/vaccine/upcoming"   
-          component={Upcomingbookings}
-        ></Route>
-          <Route
-          exact
-          path="/vaccine/RegisterDetails"   
-          component={RegisterDetails}
-        ></Route>
-         <Route
-          exact
-          path="/vaccine/UpcomingRegisterDetails"   
-          component={UpcomingRegisterDetails}
-        ></Route>
-          <Route
-          exact
-          path="/vaccine/CheckBookingDetails"   
-          component={CheckBookingDetails}
-        ></Route>
-        
+        <Route exact path="/contactTracing" component={ContactTable}></Route>
       </Switch>
     </Router>
   );
