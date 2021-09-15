621c9769adaf72c81204f6b440481c42015b0c96
nuvin0214 <64645538+nuvin0214@users.noreply.github.com>
2021-09-15 09:57:50 +0530

    nuvinn

---
+++ frontend/src/components/ContactTracingManager/SubComponenets/ContactTable.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sidebar from './ContactTracingSidebar';
+
+const ContactTable = () => {
+    return (
+        <div>
+            <Sidebar/>
+            nuvin
+        </div>
+    )
+}
+
+export default ContactTable;
